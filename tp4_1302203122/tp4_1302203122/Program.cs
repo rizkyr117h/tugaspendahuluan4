597c359780b2cc2ca5223640879d60cc59d47f34
Rizky Ramdhan Nugraha <rizkyramdhan.117h@gmail.com>
2022-03-18 13:37:09 +0700

    menambahkan class DataGeneric

--- tp4_1302203122/tp4_1302203122/Program.cs
+++ tp4_1302203122/tp4_1302203122/Program.cs
@@ -10,6 +10,27 @@ namespace tp4_1302203122
     {
         static void Main(string[] args)
         {
+            Console.Write("Massukan NIM : ");
+            string nim;
+            nim = Console.ReadLine();
+            DataGeneric<String> data = new DataGeneric<string>(nim);
+            data.Prindata();
+
         }
+
+        class DataGeneric<T>
+        {
+            private T data;
+            public DataGeneric(T data)
+            {
+                this.data = data;
+            }
+
+            public void Prindata()
+            {
+                Console.WriteLine("Data yang tersimpan adalah : " + data);
+            }
+        }
+
     }
 }
